156907b9893db6bc9e71d624b873e523316cecbe
= <=>
2020-09-01 15:49:47 +0000

    Add File 3

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'), ord ('z') + 1) if a not in (ord('e'), (ord('q'))], sep='', end='')
+for c in range(97, 123):
+    if(chr(c) != 'q' and chr(c) != 'e'):
+        print("{}".format(chr(c)), end="")
