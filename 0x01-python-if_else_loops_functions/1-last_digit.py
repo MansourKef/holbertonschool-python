35e6dc2b051f586ff0f96500cde6f7bfa6c99ca9
= <=>
2020-09-01 11:38:52 +0000

    Add File 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,4 +10,4 @@ if last_digit > 5:
 elif last_digit < 6 and last_digit != 0:
     print(message, "and is less than 6 and not 0")
 else:
-    print(message, "and is zero")
+    print(message, "and is 0")
