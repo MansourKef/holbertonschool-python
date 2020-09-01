53e8c81c69f2fab13a78b5c21f0e55078b1df655
= <=>
2020-09-01 15:19:03 +0000

    Add File 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in range(len(str)):
         if i == len(str) - 1:
             print("{}".format(chr(ord(str[i]) - 32) if ord(
-                str[i]) in range(97, 123) else str[i]))
+                str[i]) in range(97, 123) else str[i]), end='')
         else:
             print("{}".format(chr(ord(str[i]) - 32) if ord(
                 str[i]) in range(97, 123) else str[i]), end='')
