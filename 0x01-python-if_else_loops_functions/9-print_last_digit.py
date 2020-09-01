c57c4c75987d38dc841c39d98c31ffe033b558de
= <=>
2020-09-01 16:23:38 +0000

    Add File 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,5 @@ def print_last_digit(number):
     last_digit = number % 10
     if number < 0:
         last_digit = (abs(number) % 10) * -1
+    print('{:d}'.format(last_digit), end="")
     return last_digit
