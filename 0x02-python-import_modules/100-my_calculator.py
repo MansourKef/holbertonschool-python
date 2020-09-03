d0ee5a808b9b649f161d55e2815421b41f341233
= <=>
2020-09-03 18:12:47 +0000

    Add File 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4:
         sys.stderr.write('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
         exit(1)
     operator = sys.argv[2]
