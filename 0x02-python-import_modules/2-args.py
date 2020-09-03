34da9ba10328aa45783a723143baccce9293c7ff
= <=>
2020-09-03 13:45:37 +0000

    Add File 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:", sys.argv[1])
     else:
-        print("{} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(len(sys.argv) - 1))
         count = 0
-        for filearg in sys.argv:
+        for filearg in sys.argv[1:]:
             count = count + 1
             print("{}: {}".format(count, filearg))
