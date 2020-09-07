0a9d37c358ce7478be58d6eccb07f6ea1bfbc9c8
= <=>
2020-09-07 13:45:37 +0000

    Add File 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a:
+        return tuple_b
+    if not tuple_b:
+        return tuple_a
     if len(tuple_a) >= 2 & len(tuple_b) >= 2:
         tuple_c = ("({}, {})".format(
             tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
