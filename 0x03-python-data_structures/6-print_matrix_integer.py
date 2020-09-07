0c7000bd7fd6626d71f5c5421a8b9fdde431ff5d
= <=>
2020-09-07 12:39:37 +0000

    Add File 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in range(3):
+            for j in range(3):
+                print("{:d} ".format(matrix[i][j]), end="")
+            print()
