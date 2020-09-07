d81594a1b9a97cb9d15067d4617747c64a9619ee
= <=>
2020-09-07 13:32:19 +0000

    Add File 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a & tuple_b:
-        if len(tuple_a) >= 2 & len(tuple_b) >= 2:
-            tuple_c = ("({}, {})".format(
-                tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
-        if len(tuple_a) == 1:
-            tuple_c = ("({}, {})".format(tuple_a[0] + tuple_b[0], tuple_b[1]))
-        if len(tuple_b) == 1:
-            tuple_c = ("({}, {})".format(tuple_a[0] + tuple_b[0], tuple_a[1]))
-        if len(tuple_a) == 0:
-            tuple_c = tuple_b
-        if len(tuple_b) == 0:
-            tuple_c = tuple_a
+    if len(tuple_a) >= 2 & len(tuple_b) >= 2:
+        tuple_c = ("({}, {})".format(
+            tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
+    if len(tuple_a) == 1:
+        tuple_c = ("({}, {})".format(tuple_a[0] + tuple_b[0], tuple_b[1]))
+    if len(tuple_b) == 1:
+        tuple_c = ("({}, {})".format(tuple_a[0] + tuple_b[0], tuple_a[1]))
+    if len(tuple_a) == 0:
+        tuple_c = tuple_b
+    if len(tuple_b) == 0:
+        tuple_c = tuple_a
     return tuple_c
