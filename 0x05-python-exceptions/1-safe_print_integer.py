3d6c23230209952395c1c785ac79b29627f67911
= <=>
2020-09-10 13:11:00 +0000

    Add File 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value):
+        if value >= 0 or value <= 0:
             print("{:d}".format(value))
             return True
-    except ValueError:
         return False
     except:
         pass
