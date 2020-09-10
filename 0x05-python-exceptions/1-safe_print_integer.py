916ebea0dbe074645fafb244156670a7b2f58329
= <=>
2020-09-10 13:09:32 +0000

    Add File 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,7 @@ def safe_print_integer(value):
         if int(value):
             print("{:d}".format(value))
             return True
-    except:
+    except ValueError:
         return False
+    except:
+        pass
