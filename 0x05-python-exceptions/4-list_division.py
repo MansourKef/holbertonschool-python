b9b2b0b689f8424e47629330bb7f126440c1d2b8
= <=>
2020-09-10 12:50:51 +0000

    Add File 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,9 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = [None] * list_length
     for i in range(0, list_length):
         try:
-            new_list[i] = my_list_1[i] / my_list_2[i]
+            a = float(my_list_1[i])
+            b = float(my_list_2[i])
+            new_list[i] = a / b
         except ZeroDivisionError:
             print("division by 0")
         except ValueError:
