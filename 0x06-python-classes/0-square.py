346379070aedce87323821521ede1c67ec77301e
= <=>
2020-09-14 13:17:11 +0000

    Add File 0 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Square:
     """"This is An Empty Class Calles Squre"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square():
+    """This is An Square Class Based On The
+    Square Class In File 0-square.py"""
+    def __init__(self, size):
+        """This is init function of this Class
+        that sets the Value of __size"""
+        self.__size = size
