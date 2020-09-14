81979f75e4562f3590a36b675501e97144bc36a0
= <=>
2020-09-14 17:47:36 +0000

    Add File 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square():
+    """This is An Square Class Based On The
+    Square Class"""
+    def __init__(self, size=0):
+        """This is init function of this Class
+        that sets the Value of size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("ize must be >= 0")
+        else:
+            self.__size = size
