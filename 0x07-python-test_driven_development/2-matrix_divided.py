bd53713a15797bc9d48ee5de1047376ee833c262
= <=>
2020-09-16 16:44:20 +0000

    Add Test For File 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,6 +12,8 @@ def matrix_divided(matrix, div):
     """
     if matrix is None:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if any((len(matrix[0]) != len(d)) for d in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     new_matrix = [None] * len(matrix)
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
