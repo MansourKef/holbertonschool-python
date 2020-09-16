feccaa95618e120f84038dab2cbee3146fedf8f9
= <=>
2020-09-16 15:50:56 +0000

    Add Test For File 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,15 +10,14 @@ def print_square(size):
     """ Function that prints a square with the character #.
         return None
     """
-    if type(size) != int or (type(size) == float and size < 0):
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
     if size == 0:
-        return
-
+        print()
     for i in range(size):
         for j in range(size):
             print("#", end="")
