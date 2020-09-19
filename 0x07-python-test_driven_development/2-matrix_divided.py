373f9f1a881948a1fa82787ff82a4ed97fb509a9
= <=>
2020-09-19 12:00:37 +0000

    Fix PEP8 file 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,14 +11,16 @@ def matrix_divided(matrix, div):
             return new matrix
     """
     if matrix is None:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
     if any((len(matrix[0]) != len(d)) for d in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     new_matrix = [None] * len(matrix)
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if not isinstance(matrix[i][j], (float, int)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists)"
+                                " of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
