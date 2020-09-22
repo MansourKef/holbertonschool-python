e1625ef1cdc408ec8ccef0a5db3d30c3a4eb4a67
= <=>
2020-09-22 10:00:38 +0000

    Fix File 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -82,7 +82,6 @@ class Rectangle:
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
     
-    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Function that compares 2 rectangles
