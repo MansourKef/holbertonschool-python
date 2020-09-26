2628cfc0a33637a38f9bbfbc553714435ea01b28
= <=>
2020-09-26 06:24:06 +0000

    Add File

---
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    function taht returns
+    true if all the boxes will be unlocked
+    false if any box wont be unlocked
+    """
+    if len(boxes) == 1:
+        """
+        By default the first box is unlocked
+        if there is only one box then return true
+        """
+        return True
+    else:
+        """
+        Add the 0 indexed Box by default to be unlocked
+        """
+        UnlockedBoxes = []
+        if 0 not in UnlockedBoxes:
+            UnlockedBoxes.append(0)
+        """
+        get the count of the unlocked boxes
+        """
+        UnlockedBoxesCount = len(UnlockedBoxes)
+        """
+        Iterate in the inlocked boxes
+        """
+        for i in UnlockedBoxes:
+            if i in range(len(boxes)):
+                for j in range(len(boxes[i])):
+                    """
+                    add only the new unlocked boxes in the list
+                    """
+                    if boxes[i][j] not in UnlockedBoxes:
+                        UnlockedBoxes.append(boxes[i][j])
+        """
+        return the result
+        """
+        return len(UnlockedBoxes) == len(boxes)
