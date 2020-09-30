0defd0a69820f1cc278c7a15b5f9c9137983727c
= <=>
2020-09-30 10:26:13 +0000

    Add File 3

--- 0x0B-python-inheritance/3-is_kind_of_class.py
+++ 0x0B-python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    """check if obj is a_class"""
+def is_kind_of_class(obj, a_class):
+    """check if obj is is kind of a_class"""
     return isinstance(obj, a_class)
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
