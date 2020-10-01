a7031a28140e9829cd167d2e47fae01f78f2a8d9
= <=>
2020-10-01 11:00:19 +0000

    add file 2

--- 0x0C-python-input_output/2-read_lines.py
+++ 0x0C-python-input_output/2-read_lines.py
@@ -5,12 +5,9 @@ Module 2-read_lines.py
 
 
 def read_lines(filename="", nb_lines=0):
-        """reads n lines of a file in UTF8"""
-        with open(filename, encoding="utf-8") as myFile:
-            if nb_lines <=0:
-                print(myFile.read())
-            elif nb_lines >= len(myFile.readlines()):
-                print(myFile.read())
-            else:
-                for i in range(nb_lines):
-                    myFile.readline()
+    """Reads n lines of a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r') as f:
+        if nb_lines <= 0 or nb_lines >= len(open(filename).readlines()):
+            nb_lines = len(open(filename).readlines())
+        for i in range(nb_lines, 0, -1):
+            print(f.readline(), end="")
