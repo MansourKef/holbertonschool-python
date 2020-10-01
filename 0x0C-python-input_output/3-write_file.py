e37152b85a58acc6bb065d2a78a2e20afc3d62e9
= <=>
2020-10-01 09:51:15 +0000

    add file 3 4

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+B#!/usr/bin/python3
 """
 Module 3-write_file.py
 """
@@ -6,5 +6,5 @@ Module 3-write_file.py
 
 def write_file(filename="", text=""):
         """write a text in a file"""
-        with open(filename, mode="w" ,encoding="utf-8") as myFile:
+        with open(filename, mode="w", encoding="utf-8") as myFile:
             return(myFile.write(text))
