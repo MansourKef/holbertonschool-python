cd58668f374ae81517106a6f3f261dc332801918
= <=>
2020-10-12 09:26:26 +0000

    base.py

---
+++ 0x0D-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        init Class
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """change the print output"""
+        return("[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id, self.x, self.y,
+            self.height))
