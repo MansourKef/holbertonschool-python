75101cd93fcbd74dfa5c170ceafc14e0c2c8e924
= <=>
2020-10-23 14:34:21 +0000

    Add File 7

---
+++ 0x13-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- create another table in exisiting db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states (id));
