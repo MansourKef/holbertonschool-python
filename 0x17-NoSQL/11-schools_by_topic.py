54f7927eaa603b531387dfcaf3e6cc42e588f043
= <=>
2020-10-27 04:53:22 +0000

    Add File 11

--- 0x17-NoSQL/11-schools_by_topic.py
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -11,4 +11,4 @@ def schools_by_topic(mongo_collection, topic):
     """
     schools_by_topic
     """
-    mongo_collection.find({"topics": topic})
+    return mongo_collection.find({"topics": topic})
