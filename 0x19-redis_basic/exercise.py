ad8d3bce9412edca2b5394c2712c476c5a8c834f
= <=>
2020-10-27 10:50:35 +0000

    Fix file

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -17,12 +17,12 @@ class Cache:
         """
         r = redis.Redis()
         self.__redis = r
-        redis.flush(r)
+        r.flushdb()
 
     def store(self, data):
         """
         store function
         """
-        key = self.__redis.randomkey()
+        key = str(self.__redis.randomkey())
         self.__redis.set(key, data)
         return key
