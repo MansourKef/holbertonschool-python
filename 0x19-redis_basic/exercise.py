615f45a9d2baf838d26fbdc0573c4cea0bc991e3
= <=>
2020-10-27 11:05:32 +0000

    Fix file

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -7,6 +7,8 @@ Module exercise.py
 import redis
 from uuid import uuid4
 from typing import Union, Optional, Callable, List
+from sys import byteorder
+from functools import wraps
 
 
 class Cache:
@@ -27,3 +29,9 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """get method"""
+        result = self._redis.get(key)
+        return fn(result) if fn else result
