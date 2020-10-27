69c883a54c9a6c6957e37850a71c930db48979e7
= <=>
2020-10-27 12:56:26 +0000

    Add Project

---
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module 0-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter
+    """
+    d = 0
+    perimeter = 0
+    height = len(grid)
+    length = len(grid[0])
+    for line in grid:
+        c = 0
+        for val in line:
+            if val == 1:
+                surround = 4
+                if c != length - 1:
+                    if grid[d][c + 1] == 1:
+                        surround -= 1
+                if c != 0:
+                    if grid[d][c - 1] == 1:
+                        surround -= 1
+                if d != height - 1:
+                    if grid[d + 1][c] == 1:
+                        surround -= 1
+                if d != 0:
+                    if grid[d - 1][c] == 1:
+                        surround -= 1
+                perimeter += surround
+            c += 1
+        d += 1
+    return perimeter
