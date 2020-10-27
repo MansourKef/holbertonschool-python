e3f75ee29830dc9ee644b0ab55eda565680af598
= <=>
2020-10-27 11:50:12 +0000

    Add File 0

--- 0x1A-caching/0-basic_cache.py
+++ 0x1A-caching/0-basic_cache.py
@@ -7,13 +7,14 @@ class BasicCache(BaseCaching):
     """ inherits from BaseCaching and is a caching system
     """
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ 
+        """
         Get an item by key value
         """
         if key is None or self.cache_data.get(key) is None:
