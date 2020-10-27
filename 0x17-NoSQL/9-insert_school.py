9e8f84d20e7b42f6064a26449fc092236dfec310
= <=>
2020-10-27 04:11:36 +0000

    Add File 9

---
+++ 0x17-NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module 9-insert_school.py
+"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert_school
+    """
+    mongo_collection.insert({name : kwargs['name'] , address : ['address']})
+    return mongo_collection.find({name : kwargs['name']}, {"_id" : 1, "name" : 0, "address" : 0})
